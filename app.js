873ecbc59b79ccb8c23ced1511d221e3ac4de42f
umarsahab <121605324+umarsahab@users.noreply.github.com>
2023-05-06 14:29:41 +0500

    submit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+ 
+
+// Question 1
+var age=20;
+alert("My age is "+ age +" years old"  )
+
+// Question 2
+var numVisits = 0;
+
+if (numVisits === null) {
+  numVisits = 1;
+} else {
+
+  numVisits = numVisits + 1;
+}
+alert("You have visited this site" + numVisits+ " times."  );
+
+// Question 3
+var birthYear=2002;
+document.write("my birth year is "+ birthYear)
+document.write("data type of my decleared variable is "+ typeof(birthYear) + "<br />")
+
+// Question 4
+var visitorName = "Muhammad Umar"
+var productTitle = "T-Shirts"
+var Quantity = 5
+
+document.write(visitorName +" ordered " + Quantity +" " + productTitle +" on xy Clothing stores" )
